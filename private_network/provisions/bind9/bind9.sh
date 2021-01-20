cc8e70ac59a72fb6239fbbacb498fb5d1d8acf8d
Leddzip <leddzip@gmail.com>
2021-01-20 23:10:58 +0100

    feature/dns-resolution
    trying to make a dns server on balancer node using bind9

---
+++ private_network/provisions/bind9/bind9.sh
@@ -0,0 +1,18 @@
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install -y bind9 dnsutils
+
+sudo ufw allow "Bind9"
+sudo ufw reload
+
+# managing files
+sudo mkdir /etc/bind/namespace
+sudo mkdir /etc/bind/zones
+
+sudo cp /vagrant/provisions/bind9/named.conf.options /etc/bind/
+sudo cp /vagrant/provisions/bind9/named.conf.local /etc/bind/
+sudo cp /vagrant/provisions/bind9/leddzip.io.zone /etc/bind/namespace/
+sudo cp /vagrant/provisions/bind9/db.192 /etc/bind/zones/
+
+sudo systemctl restart bind9
