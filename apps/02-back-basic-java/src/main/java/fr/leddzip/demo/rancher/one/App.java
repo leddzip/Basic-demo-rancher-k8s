7cd36510c030319020cbc65e2bc91174a1a01e19
Leddzip <leddzip@gmail.com>
2021-02-11 23:54:38 +0100

    feature/basic-backend
    move front-one into apps folder and create a new basic java backend inside de apps folder

---
+++ apps/02-back-basic-java/src/main/java/fr/leddzip/demo/rancher/one/App.java
@@ -0,0 +1,13 @@
+package fr.leddzip.demo.rancher.one;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
