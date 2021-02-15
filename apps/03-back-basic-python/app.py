413033a63fc205e2c60df7714403da7b10b5cb76
Leddzip <leddzip@gmail.com>
2021-02-15 02:16:10 +0100

    feature/python-simple-back
    basic backend with cors support

---
+++ apps/03-back-basic-python/app.py
@@ -0,0 +1,20 @@
+import os
+import uuid
+import waitress
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/api/v1/random')
+def welcome():
+    return jsonify({
+        'uuid': str(uuid.uuid4())
+    })
+
+
+if __name__ == '__main__':
+    waitress.serve(app, listen='*:'+os.getenv('PORT'))
