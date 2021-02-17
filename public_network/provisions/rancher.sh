65d4e1995e26ef62794946d86d5163dd8ca7d086
Leddzip <leddzip@gmail.com>
2021-02-17 16:58:27 +0100

    restructuring
    removing the public_network version of the infra
    
    moving kubernetes related files inside a dedicated root folder

--- public_network/provisions/rancher.sh
+++
@@ -1,2 +0,0 @@
-# Starting rancher in a docker container (to persist data, we can use volume: -v /data:/var/lib/rancher)
-sudo docker run -d --privileged --restart=always -p 80:80 -p 443:443 rancher/rancher
